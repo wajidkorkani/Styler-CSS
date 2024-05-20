ad6f2b093d578bdd1bb9bb625de1566cb82ab951
Linux <linux@kali.kali>
2024-05-20 11:55:17 -0400

    Developer can change text-color on hover using the html class like this hover:text-NameOfColorHere.

--- Src/hover.js
+++ Src/hover.js
@@ -13,6 +13,18 @@ bgColor.forEach(element => {
   }
 });
 
+
+let TextColor = document.querySelectorAll('[class*=hover\\:text-]');
+bgColor.forEach(element => {
+  let smcolorClass = element.className.match(/hover\:text\-(\S+)/);
+  if (smcolorClass && smcolorClass.length > 1) {
+    let colorName = smcolorClass[1];
+    css += `:hover.hover\\:text-${colorName} { color: ${colorName}; }\n`;
+  }
+});
+
+
+
 // Generated CSS rules for classes hover:rounded-1 to hover:rounded-1000
 for (let i = 1; i <= 1000; i++) {
   css += `.hover\\:rounded-${i}:hover { border-radius: ${i}px; }\n`;
