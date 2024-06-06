cd35ce234b8a661e976d1d308b04e1a2445440f7
Linux <linux@kali.kali>
2024-06-06 15:12:48 +0500

    Media query classes names are fixed in media_query.js file.

--- Src/media_query.js
+++ Src/media_query.js
@@ -1,47 +1,47 @@
 
     let smt = '';
-    let bgElementsC = document.querySelectorAll('[class*=sm\\:t-]');
+    let bgElementsC = document.querySelectorAll('[class*=small\\:text-]');
     bgElementsC.forEach(element => {
-        let smcolorClass = element.className.match(/sm\:t\-(\S+)/);
+        let smcolorClass = element.className.match(/small\:text\-(\S+)/);
         if (smcolorClass && smcolorClass.length > 1) {
             let colorName = smcolorClass[1];
-            smt += `@media (max-width: 767px) { .sm\\:t-${colorName} { color: ${colorName}; } }\n`;
+            smt += `@media (max-width: 767px) { .small\\:text-${colorName} { color: ${colorName}; } }\n`;
         }
     });
 
 
     // Dynamic CSS generation based on the color class in HTML markup
     let mdt = '';
-    let mdbgElementsC = document.querySelectorAll('[class*=md\\:t-]');
+    let mdbgElementsC = document.querySelectorAll('[class*=medium\\:text-]');
     mdbgElementsC.forEach(element => {
-        let mdcolorClass = element.className.match(/md\:t\-(\S+)/);
+        let mdcolorClass = element.className.match(/medium\:text\-(\S+)/);
         if (mdcolorClass && mdcolorClass.length > 1) {
             let colorName = mdcolorClass[1];
-            mdt += `@media (min-width: 768px) { .md\\:t-${colorName} { color: ${colorName}; } }\n`;
+            mdt += `@media (min-width: 768px) { .medium\\:text-${colorName} { color: ${colorName}; } }\n`;
         }
     });
 
 
     // Dynamic CSS generation based on the color class in HTML markup
     let lgt = '';
-    let lgbgElementsC = document.querySelectorAll('[class*=lg\\:t-]');
+    let lgbgElementsC = document.querySelectorAll('[class*=large\\:text-]');
     lgbgElementsC.forEach(element => {
-        let lgcolorClass = element.className.match(/lg\:t\-(\S+)/);
+        let lgcolorClass = element.className.match(/large\:text\-(\S+)/);
         if (lgcolorClass && lgcolorClass.length > 1) {
             let colorName = lgcolorClass[1];
-            lgt += `@media (min-width: 992px) { .lg\\:t-${colorName} { color: ${colorName}; } }\n`;
+            lgt += `@media (min-width: 992px) { .large\\:text-${colorName} { color: ${colorName}; } }\n`;
         }
     });
 
 
     // Dynamic CSS generation based on the color class in HTML markup
     let xlt = '';
-    let xlbgElementsC = document.querySelectorAll('[class*=xl\\:t-]');
+    let xlbgElementsC = document.querySelectorAll('[class*=xl\\:text-]');
     xlbgElementsC.forEach(element => {
-        let xlcolorClass = element.className.match(/xl\:t\-(\S+)/); // Corrected from sm to xl
+        let xlcolorClass = element.className.match(/xl\:text\-(\S+)/); // Corrected from sm to xl
         if (xlcolorClass && xlcolorClass.length > 1) {
             let colorName = xlcolorClass[1];
-            xlt += `@media (min-width: 1200px) { .xl\\:t-${colorName} { color: ${colorName}; } }\n`; // Corrected from sm to xl
+            xlt += `@media (min-width: 1200px) { .xl\\:text-${colorName} { color: ${colorName}; } }\n`; // Corrected from sm to xl
         }
     });
 
